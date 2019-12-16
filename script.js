eadffadc4bff66986217cc4c4522663d7e1432ae
Dylan Mahaffey <dylan.mahaffey@gmail.com>
2019-12-16 08:57:10 -0800

    connected buttons and added js for the plan and email params

--- script.js
+++ script.js
@@ -2,11 +2,22 @@ const pro = document.getElementById('pro');
 const basic = document.getElementById('basic');
 const enterprise = document.getElementById('enterprise');
 
+const email = document.getElementById('email');
+let planSelected = 'pro';
+
+
 function activateButton(id){
     pro.classList.remove('active');
     basic.classList.remove('active');
     enterprise.classList.remove('active');
 
-    console.log(id);
     document.getElementById(id).classList.add('active');
+    planSelected = id;
+}
+
+function send(){
+    const emailValue = btoa(email.value);
+    const queryString = `?email=${emailValue}&plan=${planSelected}`;
+    
+    window.location = "https://happybeansdesign.com/payment" + queryString;
 }
